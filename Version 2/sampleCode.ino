7b49afbc1072d683c752567a699386bc285af081
Imber06 <dahlmk06@gmail.com>
2025-01-08 20:39:04 -0700

    Create sampleCode.ino

---
+++ Version 2/sampleCode.ino
@@ -0,0 +1,13 @@
+/* 
+  This code serves as a sample of how our arm might operate.
+  As such, I'll be leaving out specifics. We're hoping to
+  control the arm with this code via an Arduino or a similar device.
+*/
+
+void setup() {
+  
+}
+
+void loop() {
+  
+}
